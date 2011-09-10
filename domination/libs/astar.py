321042211cb1ce59997741c60a68ac1690f68c0c
noio <paranoid.ndroid@gmail.com>
2011-09-10 14:33:02 +0300

    Added link for astar library.

--- domination/libs/astar.py
+++ domination/libs/astar.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2008 Mikael Lind
 #
+# From https://github.com/elemel/python-astar
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
