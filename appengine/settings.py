7be27f35a4935291995009d9380781100f61ccd3
Thomas van den Berg <paranoid.ndroid@gmail.com>
2012-01-22 17:58:00 +0100

    remove gaesessions

--- appengine/settings.py
+++ appengine/settings.py
@@ -12,7 +12,6 @@ MIDDLEWARE_CLASSES = (
     #'firepython.middleware.FirePythonDjango',
     #'appstats.recording.AppStatsDjangoMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'gaesessions.DjangoSessionMiddleware',
     'dominationgame.middleware.AddUserToRequestMiddleware',
     'dominationgame.middleware.ScopeToGroupMiddleware',
     #'django.middleware.http.ConditionalGetMiddleware',
