3f81351a653f157f79c0abbe561b1b55b1d024d2
Thomas van den Berg <paranoid.ndroid@gmail.com>
2012-03-22 15:05:24 +0100

    allow disabling of replays in tournament

--- domination/tournament.py
+++ domination/tournament.py
@@ -75,7 +75,7 @@ def run_games(gamekwargs, max_threads=2):
     
 def full(agents=[], settings=Settings(), field=FieldGenerator(), 
         repeats=2, swap=True, folder=None, output_folder=None, draw_margin=0.05,
-        max_threads=2):
+        max_threads=2, save_all=True):
     """ Runs a full tournament between given agents. 
     """
     if folder is not None:
@@ -111,8 +111,9 @@ def full(agents=[], settings=Settings(), field=FieldGenerator(),
     csvf = csv.DictWriter(open(fn+'_games.csv','w'), fieldnames, extrasaction='ignore')
     csvf.writerow(dict(zip(fieldnames, fieldnames)))
     # Create a zip with the replays
-    repzip = zipfile.ZipFile(fn+'_replays.zip','w')
-    logzip = zipfile.ZipFile(fn+'_logs.zip','w')
+    if save_all:
+        repzip = zipfile.ZipFile(fn+'_replays.zip','w')
+        logzip = zipfile.ZipFile(fn+'_logs.zip','w')
     # Remove the common prefix from agent filenames
     prefix = os.path.commonprefix(agents).rfind('/') + 1
     games = []
@@ -129,11 +130,13 @@ def full(agents=[], settings=Settings(), field=FieldGenerator(),
         # Write a replay
         r = os.path.splitext(os.path.basename(r))[0]
         b = os.path.splitext(os.path.basename(b))[0]
-        repzip.writestr('replay_%04d_%s_vs_%s.pickle'%(i, r, b), pickle.dumps(replay, pickle.HIGHEST_PROTOCOL))
-        logzip.writestr('log_%04d_%s_vs_%s.txt'%(i,r,b), log.truncated(kbs=32))
+        if save_all:
+            repzip.writestr('replay_%04d_%s_vs_%s.pickle'%(i, r, b), pickle.dumps(replay, pickle.HIGHEST_PROTOCOL))
+            logzip.writestr('log_%04d_%s_vs_%s.txt'%(i,r,b), log.truncated(kbs=32))
         
-    repzip.close()
-    logzip.close()
+    if save_all:
+        repzip.close()
+        logzip.close()
         
     # Write summary
     by_match = defaultdict(lambda: defaultdict(int))
@@ -170,6 +173,7 @@ if __name__ == '__main__':
     parser.add_option("-r", "--repeats", help="Number of times to repeat each game [default: %default]", default=2)
     parser.add_option("-t", "--threads", help="How many threads to use [default: %default]", default=2)    
     parser.add_option("-x", "--noswap", help="Disable swapping of red/blue teams", action="store_false", dest='swap', default=True)
+    parser.add_option("-c", "--compact", help="Disable saving of replays/logs to save space", action="store_true", dest='compact', default=False)
     parser.add_option("-s", "--settings", help="Python dict of settings as a string [default: %default]", default="{}")
     (options, args) = parser.parse_args()
     if len(sys.argv) < 2:
@@ -180,4 +184,4 @@ if __name__ == '__main__':
     print settings
     full(folder=options.folder, settings=settings, 
          repeats=int(options.repeats), swap=options.swap,
-         max_threads=int(options.threads))
+         max_threads=int(options.threads), save_all=not options.compact)
